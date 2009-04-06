df2588090d5b80e7eb04e2077100b2c5bbb485ae
liebster <liebster@quintagroup.com>
2009-04-06 13:25:48 +0000

    Add install script and uninstall browserlayer

--- quintagroup/plonecomments/Extensions/Install.py
+++ quintagroup/plonecomments/Extensions/Install.py
@@ -1,6 +1,13 @@
 from Products.CMFCore.utils import getToolByName
 from StringIO import StringIO
 
+def install(self):
+    out = StringIO()
+    setup_tool = getToolByName(self, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-quintagroup.plonecomments:default')
+    print >> out, "Imported install profile."
+    return out.getvalue()
+
 def uninstall(self):
     out = StringIO()
     setup_tool = getToolByName(self, 'portal_setup')
