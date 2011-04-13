7d958509f1fedd276295709642f40d865df67ff0
kroman0 <kroman0@quintagroup.com>
2011-04-13 11:04:55 +0000

    Fix for qcomments4.js: compatibility with plone3

--- quintagroup/plonecomments/skins/quintagroup_plonecomments/qcomments4.js
+++ quintagroup/plonecomments/skins/quintagroup_plonecomments/qcomments4.js
@@ -1,4 +1,12 @@
+var common_content_filter = '#content>*:not(div.configlet),dl.portalMessage.error,dl.portalMessage.info';
 $(document).ready(function() {
+    function redirectbasehref(el, responseText) {
+        var mo = responseText.match(/<base href="(.+?)"/i);
+        if (mo.length === 2) {
+            return mo[1];
+        }
+        return location;
+    }
     function noformerrorshow(el, noform) {
         var o = $(el);
         var emsg = o.find('dl.portalMessage.error');
@@ -12,10 +20,10 @@ $(document).ready(function() {
     $('div.comment form[name=edit], div.comment form[name=reply], div.comment form[name=report_abuse]').prepOverlay(
         {
             subtype: 'ajax',
-            filter: '#content>*',
+            filter: common_content_filter,//'#content>*',
             formselector: 'form[name="edit_form"]',
             noform: function(el) {return noformerrorshow(el, 'redirect');},
-            redirect: function () {return location.href;},
+            redirect: redirectbasehref,
             closeselector: '[name=form.button.Cancel]'
         }
     );
